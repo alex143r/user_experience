cc1a35d1ea3ee2be97f3f6b747c8499998ad3d74
Alexander <alex143r@stud.kea.dk>
2021-10-06 13:48:03 +0200

    tax calc

---
+++ tax_calculator/tax_calculator.js
@@ -0,0 +1,25 @@
+function calculateTax() {
+  let amount = document.querySelector("#amount");
+  let tax = document.querySelector("#tax");
+
+  if (tax.value > 100) {
+    tax.value = 100.0;
+  }
+
+  let amountValue = amount.value;
+  let taxValue = tax.value / 100;
+  let taxResult = amountValue * taxValue;
+  let finalAmount = amountValue - taxResult;
+
+  document.querySelector(".tax-amount").innerHTML = taxResult.toFixed(2);
+  document.querySelector(".final-amount").innerHTML = finalAmount.toFixed(2);
+}
+
+document.querySelector("button").addEventListener("click", function (e) {
+  const form = document.querySelector("form");
+  e.preventDefault();
+  if (form.checkValidity()) {
+    calculateTax();
+  }
+  form.reportValidity();
+});
